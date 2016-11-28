3a3718fe5cac4674a9f044159bcb21a0d294eb71
Matthew Shim <umattdub@gmail.com>
2016-11-27 23:46:44 -0800

    implemented redux

---
+++ js/Store.jsx
@@ -0,0 +1,43 @@
+const redux = require('redux')
+const reactRedux = require('react-redux')
+
+const SET_SEARCH_TERM = 'setSearchTerm'
+const initialState = {
+  searchTerm: ''
+}
+
+const rootReducer = (state, action) => {
+  if (!state) {
+    state = initialState
+  }
+  switch (action.type) {
+    case SET_SEARCH_TERM:
+      return reduceSearchTerm(state, action)
+    default:
+      return state
+  }
+}
+
+const reduceSearchTerm = (state, action) => {
+  const newState = {}
+  Object.assign(newState, state, {searchTerm: action.value})
+  return newState
+}
+
+const store = redux.createStore(rootReducer)
+
+const mapStateToProps = (state) => {
+  return {searchTerm: state.searchTerm}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setSearchTerm (searchTerm) {
+      dispatch({type: SET_SEARCH_TERM, value: searchTerm})
+    }
+  }
+}
+
+const connector = reactRedux.connect(mapStateToProps, mapDispatchToProps)
+// { connector: connector, store: store }, thanks ES6 shortcuts
+module.exports = { connector, store }
